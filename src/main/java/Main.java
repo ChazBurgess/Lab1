6a811276e01caeea1d421a4889e523f52d7360d0
4chaz <4chaz@DESKTOP-KPJR5NU>
2019-09-06 18:53:31 -0500

    Number 7 Sales Tax

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author 4chaz
- */
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+//7. Sales Tax
 public class Main {
-    
+
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        NumberFormat formatter = NumberFormat.getCurrencyInstance();
+
+        System.out.println("Please enter the amount of the purchase ");
+
+        float amountOfPurchase = keyboard.nextFloat();
+        double stateTax = .04;
+        double countyTax = .02;
+        double totalSalesTax = stateTax + countyTax;
+        double totalPurchaseAmount = (amountOfPurchase * totalSalesTax) + amountOfPurchase;
+        
+
+        System.out.println("Amount of the Purchase: " + amountOfPurchase + "\n"
+                + "State Sales Tax: " + stateTax + "\n" + "County Sales Tax: " + countyTax
+                + "\n" + "Total Sales Tax: " + totalSalesTax + "\n" + "Total amount of the sale: " 
+                + formatter.format(totalPurchaseAmount));
+     
+     
+    }
+
 }
